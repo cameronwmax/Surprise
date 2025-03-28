abaea0672421c872ec104bf9c5748319aadf1a1b
Cameron Maxwell <cameronwmax@icloud.com>
2025-03-28 11:02:24 -0700

    Fix more bugs with walking into edges

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const container = document.querySelector(".card");
 
 const leftEdge = 0;
 const rightEdge = container.offsetWidth - 33;
-const topEdge = 0;
-const bottomEdge = container.offsetHeight - 33;
+const bottomEdge = 0;
+const topEdge = container.offsetHeight - 25;
 
 let curX = cat.style.left;
 let curY = cat.style.bottom;
@@ -14,12 +14,11 @@ function moveCat() {
   let moveDir = Math.random();
 
   if (curX === leftEdge) moveDir = 0.49;
-  if (curX === rightEdge - 33) moveDir = 0.24;
-  if (curY === topEdge + 33) moveDir = 0.98;
-  if (curY === bottomEdge - 33) moveDir = 0.74;
+  if (curX === rightEdge) moveDir = 0.24;
+  if (curY === topEdge) moveDir = 0.98;
+  if (curY === bottomEdge) moveDir = 0.74;
 
   let newX = curX;
-  let newY = curY;
 
   if (moveDir < 0.25) {
     newX = Math.max(curX - moveDist, 0);
@@ -32,13 +31,13 @@ function moveCat() {
     cat.classList.add("shadow-slide");
     cat.classList.remove("shadow-vertical");
   } else if (moveDir < 0.75) {
-    newY = Math.max(newY - moveDist, topEdge);
-    cat.style.animation = "walk-down-frames 0.5s steps(3) infinite";
+    newY = Math.min(newY + moveDist, topEdge);
+    cat.style.animation = "walk-up-frames 0.5s steps(3) infinite";
     cat.classList.remove("shadow-slide");
     cat.classList.add("shadow-vertical");
   } else if (moveDir < 0.99) {
-    newY = Math.min(curY + moveDist, bottomEdge);
-    cat.style.animation = "walk-up-frames 0.5s steps(3) infinite";
+    newY = Math.max(curY - moveDist, bottomEdge);
+    cat.style.animation = "walk-down-frames 0.5s steps(3) infinite";
     cat.classList.remove("shadow-slide");
     cat.classList.add("shadow-vertical");
   }
@@ -58,9 +57,9 @@ function moveCat() {
     } else if (moveDir < 0.5) {
       cat.style.backgroundImage = "url('./imgs/cat-frames/cat-right-2.png')";
     } else if (moveDir < 0.75) {
-      cat.style.backgroundImage = "url('./imgs/cat-frames/cat-down-2.png')";
-    } else if (moveDir < 0.99) {
       cat.style.backgroundImage = "url('./imgs/cat-frames/cat-up-2.png')";
+    } else if (moveDir < 0.99) {
+      cat.style.backgroundImage = "url('./imgs/cat-frames/cat-down-2.png')";
     }
 
     setTimeout(moveCat, Math.random() * 3000 + 1000);
