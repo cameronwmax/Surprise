54144ab5956a0ff95e4e3b63d39f7f491e32b340
Cameron Maxwell <cameronwmax@icloud.com>
2025-03-28 09:03:05 -0700

    Add walk left and walk right logic

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-document.querySelector(".btn").addEventListener("click", () => {
-  const cat = document.querySelector(".animal");
+const cat = document.querySelector(".animal");
+const container = document.querySelector(".card");
 
-  cat.style.animation = "walk-frames .5s steps(3) infinite, walk 8s linear infinite";
+const leftEdge = 0;
+const rightEdge = container.offsetWidth - 33;
+const topEdge = 0;
+const bottomEdge = container.offsetHeight;
 
-  // setTimeout(() => {
-  //   cat.style.backgroundImage = "url(./imgs/cat-frames/cat-right-2.png)";
-  // }, 1000);
+let curX = leftEdge;
+let curY = bottomEdge;
+// console.log(rightEdge);
 
-  // setTimeout(() => {
-  //   cat.style.backgroundImage = "url(./imgs/cat-frames/cat-right-3.png)";
-  // }, 2000);
-});
+function moveCat() {
+  let moveDist = Math.floor(Math.random() * 150 + 50);
+  let moveDir = Math.random();
+  moveDir = 0.2;
+
+  console.log(rightEdge);
+  if (curX === leftEdge) moveDir = 0.49;
+  if (curX === rightEdge) moveDir = 0.24;
+
+  let newX = curX;
+  let newY = curY;
+  console.log(curX + moveDist);
+
+  if (moveDir < 0.25) {
+    newX = Math.max(curX - moveDist, 0);
+    cat.style.animation = "walk-left-frames 0.5s steps(3) infinite";
+  } else if (moveDir < 0.9) {
+    newX = Math.min(curX + moveDist, rightEdge);
+    cat.style.animation = "walk-right-frames 0.5s steps(3) infinite";
+  }
+
+  cat.style.transition = "left 2s linear";
+  cat.style.left = newX + "px";
+}
+moveCat();
