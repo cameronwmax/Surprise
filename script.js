f4bbed6a6ace99196c92ec8d21e44d41be7c91e0
Cameron Maxwell <cameronwmax@icloud.com>
2025-03-28 10:11:51 -0700

    Add dynamic shadow for vertical/horizontal

--- script.js
+++ script.js
@@ -24,15 +24,23 @@ function moveCat() {
   if (moveDir < 0.25) {
     newX = Math.max(curX - moveDist, 0);
     cat.style.animation = "walk-left-frames 0.5s steps(3) infinite";
+    cat.classList.add("shadow-slide");
+    cat.classList.remove("shadow-vertical");
   } else if (moveDir < 0.5) {
     newX = Math.min(curX + moveDist, rightEdge);
     cat.style.animation = "walk-right-frames 0.5s steps(3) infinite";
+    cat.classList.add("shadow-slide");
+    cat.classList.remove("shadow-vertical");
   } else if (moveDir < 0.75) {
     newY = Math.max(newY - moveDist, topEdge);
     cat.style.animation = "walk-down-frames 0.5s steps(3) infinite";
+    cat.classList.remove("shadow-slide");
+    cat.classList.add("shadow-vertical");
   } else if (moveDir < 0.99) {
     newY = Math.min(curY + moveDist, bottomEdge);
     cat.style.animation = "walk-up-frames 0.5s steps(3) infinite";
+    cat.classList.remove("shadow-slide");
+    cat.classList.add("shadow-vertical");
   }
 
   cat.style.transition = "left 2s linear, bottom 2s linear";
