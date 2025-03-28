ce788c2cecbc0502f02102e19625e7ab6af365a5
Cameron Maxwell <cameronwmax@icloud.com>
2025-03-28 12:04:54 -0700

    Add second cat, refactor moveCat to work for both cats

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const cat = document.querySelector(".animal");
+const whiteCat = document.querySelector(".white-cat");
+const blackCat = document.querySelector(".black-cat");
 const container = document.querySelector(".card");
 
 const leftEdge = 0;
@@ -6,10 +7,13 @@ const rightEdge = container.offsetWidth - 33;
 const bottomEdge = 0;
 const topEdge = container.offsetHeight - 25;
 
-let curX = cat.style.left;
-let curY = cat.style.bottom;
+let whiteCatX = whiteCat.style.left;
+let whiteCatY = whiteCat.style.bottom;
 
-function moveCat() {
+let blackCatX = blackCat.style.left;
+let blackCatY = blackCat.style.bottom;
+
+function moveCat(cat, curX, curY, type) {
   let moveDist = Math.floor(Math.random() * 150) + 50;
   let moveDir = Math.random();
 
@@ -19,27 +23,28 @@ function moveCat() {
   if (curY === bottomEdge) moveDir = 0.74;
 
   let newX = curX;
+  let newY = curY;
 
   if (moveDir < 0.25) {
     newX = Math.max(curX - moveDist, 0);
-    cat.style.animation = "walk-left-frames 0.5s steps(3) infinite";
-    cat.classList.add("shadow-slide");
-    cat.classList.remove("shadow-vertical");
+    cat.style.animation = `${type}-cat-left-frames 0.5s steps(3) infinite`;
+    cat.classList.add(`${type}-cat-shadow-horizontal`);
+    cat.classList.remove(`${type}-cat-shadow-vertical`);
   } else if (moveDir < 0.5) {
     newX = Math.min(curX + moveDist, rightEdge);
-    cat.style.animation = "walk-right-frames 0.5s steps(3) infinite";
-    cat.classList.add("shadow-slide");
-    cat.classList.remove("shadow-vertical");
+    cat.style.animation = `${type}-cat-right-frames 0.5s steps(3) infinite`;
+    cat.classList.add(`${type}-cat-shadow-horizontal`);
+    cat.classList.remove(`${type}-cat-shadow-vertical`);
   } else if (moveDir < 0.75) {
     newY = Math.min(newY + moveDist, topEdge);
-    cat.style.animation = "walk-up-frames 0.5s steps(3) infinite";
-    cat.classList.remove("shadow-slide");
-    cat.classList.add("shadow-vertical");
+    cat.style.animation = `${type}-cat-up-frames 0.5s steps(3) infinite`;
+    cat.classList.add(`${type}-cat-shadow-vertical`);
+    cat.classList.remove(`${type}-cat-shadow-horizontal`);
   } else if (moveDir < 0.99) {
     newY = Math.max(curY - moveDist, bottomEdge);
-    cat.style.animation = "walk-down-frames 0.5s steps(3) infinite";
-    cat.classList.remove("shadow-slide");
-    cat.classList.add("shadow-vertical");
+    cat.style.animation = `${type}-cat-down-frames 0.5s steps(3) infinite`;
+    cat.classList.add(`${type}-cat-shadow-vertical`);
+    cat.classList.remove(`${type}-cat-shadow-horizontal`);
   }
 
   cat.style.transition = "left 2s linear, bottom 2s linear";
@@ -53,16 +58,18 @@ function moveCat() {
     cat.style.animation = "none";
 
     if (moveDir < 0.25) {
-      cat.style.backgroundImage = "url('./imgs/cat-frames/cat-left-2.png')";
+      cat.style.backgroundImage = `url('./imgs/cat-frames/${type}-cat-left-2.png')`;
     } else if (moveDir < 0.5) {
-      cat.style.backgroundImage = "url('./imgs/cat-frames/cat-right-2.png')";
+      cat.style.backgroundImage = `url('./imgs/cat-frames/${type}-cat-right-2.png')`;
     } else if (moveDir < 0.75) {
-      cat.style.backgroundImage = "url('./imgs/cat-frames/cat-up-2.png')";
+      cat.style.backgroundImage = `url('./imgs/cat-frames/${type}-cat-up-2.png')`;
     } else if (moveDir < 0.99) {
-      cat.style.backgroundImage = "url('./imgs/cat-frames/cat-down-2.png')";
+      cat.style.backgroundImage = `url('./imgs/cat-frames/${type}-cat-down-2.png')`;
     }
 
-    setTimeout(moveCat, Math.random() * 3000 + 1000);
+    setTimeout(() => moveCat(cat, curX, curY, type), Math.random() * 3000 + 1000);
   }, 2000);
 }
-moveCat();
+
+setTimeout(() => moveCat(whiteCat, whiteCatX, whiteCatY, "white"), Math.random() * 3000 + 1000);
+setTimeout(() => moveCat(blackCat, blackCatX, blackCatY, "black"), Math.random() * 3000 + 1000);
